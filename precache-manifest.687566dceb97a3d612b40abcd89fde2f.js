2cab811b9f2c675b47151b0abb0ee09159e426e5
Sanni Lares <sanni.lares@gmail.com>
2021-03-01 12:37:05 +0200

    Updated static files

---
+++ precache-manifest.687566dceb97a3d612b40abcd89fde2f.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e58e7bb3da3add09cad57bfdd85d3ff",
+    "url": "/sannilares.github.io/index.html"
+  },
+  {
+    "revision": "9689976a8ea1a1b82549",
+    "url": "/sannilares.github.io/static/css/main.5804aa1a.chunk.css"
+  },
+  {
+    "revision": "b1e6583f891d135ddb74",
+    "url": "/sannilares.github.io/static/js/2.cb4502fe.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/sannilares.github.io/static/js/2.cb4502fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9689976a8ea1a1b82549",
+    "url": "/sannilares.github.io/static/js/main.2d387fd6.chunk.js"
+  },
+  {
+    "revision": "22269efcd1de5ebf0883",
+    "url": "/sannilares.github.io/static/js/runtime-main.aa6a41d5.js"
+  },
+  {
+    "revision": "8e513d85a4ad02629f59ea94bca50bec",
+    "url": "/sannilares.github.io/static/media/Lopputyö.8e513d85.png"
+  },
+  {
+    "revision": "3e6e748d1153511eb7fd0ed5808b946d",
+    "url": "/sannilares.github.io/static/media/Nettisivut.3e6e748d.png"
+  },
+  {
+    "revision": "88d4029253c054559c291fce8c95966c",
+    "url": "/sannilares.github.io/static/media/Nettisivut3.88d40292.png"
+  },
+  {
+    "revision": "bf39e7824b47bd17b8200a9b65bd39c5",
+    "url": "/sannilares.github.io/static/media/Nettisivut4.bf39e782.png"
+  },
+  {
+    "revision": "0bb387a6e6ebdc49c7f7a705e585ee46",
+    "url": "/sannilares.github.io/static/media/Nettisivut5.0bb387a6.png"
+  },
+  {
+    "revision": "d844cb4356b2a2eb96daf6d39d15b4af",
+    "url": "/sannilares.github.io/static/media/Nettisivut6.d844cb43.png"
+  },
+  {
+    "revision": "33c60bff058aa3c99ef632a30f73065b",
+    "url": "/sannilares.github.io/static/media/Nettisivut7.33c60bff.png"
+  },
+  {
+    "revision": "96c22f2f78904a572cd1990bd7b5da92",
+    "url": "/sannilares.github.io/static/media/Portfolio.96c22f2f.png"
+  },
+  {
+    "revision": "02d2915381eefa8460203ea5c1c8aa80",
+    "url": "/sannilares.github.io/static/media/Portfolio2.02d29153.png"
+  },
+  {
+    "revision": "6a8484e01bc8e4190a59dc8862c3cc3a",
+    "url": "/sannilares.github.io/static/media/Portfolio3.6a8484e0.png"
+  },
+  {
+    "revision": "befda96cca5d6bec3903f7bcdd7403fa",
+    "url": "/sannilares.github.io/static/media/Portfolio4.befda96c.png"
+  },
+  {
+    "revision": "c036ffcc6ff5020946aa710d0266ed67",
+    "url": "/sannilares.github.io/static/media/Portfolio5.c036ffcc.png"
+  },
+  {
+    "revision": "d11e9d3f30bdb690aab5e784a35062ce",
+    "url": "/sannilares.github.io/static/media/Portfolio6.d11e9d3f.png"
+  },
+  {
+    "revision": "39c0c91885585d9e4420a9de3d8cad80",
+    "url": "/sannilares.github.io/static/media/Portfolio7.39c0c918.png"
+  },
+  {
+    "revision": "a1f2aaa6fbfd32760a7222ddc9d49186",
+    "url": "/sannilares.github.io/static/media/PortfolioTieto.a1f2aaa6.png"
+  },
+  {
+    "revision": "aa3e2eafdca18075a21848c753e54f0a",
+    "url": "/sannilares.github.io/static/media/PortfolioTieto2.aa3e2eaf.png"
+  },
+  {
+    "revision": "7a0a36cccd34ab92b7309077b9da8b6c",
+    "url": "/sannilares.github.io/static/media/RoskApp2.7a0a36cc.png"
+  },
+  {
+    "revision": "fc7ae5e41096f33add870f48608c63c2",
+    "url": "/sannilares.github.io/static/media/Ryhmäkuva_2019-min.fc7ae5e4.jpg"
+  },
+  {
+    "revision": "75f05c158392cde8e0efc3a2fa5854ad",
+    "url": "/sannilares.github.io/static/media/Tuotekuva-Asetelma-154-min.75f05c15.jpg"
+  },
+  {
+    "revision": "282c0ca0e92ddafc6f21b87e7471b39e",
+    "url": "/sannilares.github.io/static/media/Väestöliitto2.282c0ca0.png"
+  },
+  {
+    "revision": "2c521a248520df474dcd295425bf7077",
+    "url": "/sannilares.github.io/static/media/Väestöliitto3.2c521a24.png"
+  },
+  {
+    "revision": "f3b7d629c80a85788fe1cc7377576fec",
+    "url": "/sannilares.github.io/static/media/call-24px.f3b7d629.svg"
+  },
+  {
+    "revision": "8cf396fbe991de6d0988c5f0f48a4e37",
+    "url": "/sannilares.github.io/static/media/kamera-min.8cf396fb.JPG"
+  },
+  {
+    "revision": "c39e9b930be48bec261af290c918398f",
+    "url": "/sannilares.github.io/static/media/kauneus.c39e9b93.webm"
+  },
+  {
+    "revision": "7c86c6ea4a23d1747b756e58de3d3973",
+    "url": "/sannilares.github.io/static/media/mail_outline-24px.7c86c6ea.svg"
+  },
+  {
+    "revision": "b414a0ccaea7397dea12dfcb09a2e865",
+    "url": "/sannilares.github.io/static/media/minä.b414a0cc.jpg"
+  },
+  {
+    "revision": "9348b2a4e5540926d70a9b2128ae7107",
+    "url": "/sannilares.github.io/static/media/muoti-min.9348b2a4.JPG"
+  },
+  {
+    "revision": "cb39b20333201e949957f192884fad23",
+    "url": "/sannilares.github.io/static/media/portaat-min.cb39b203.JPG"
+  },
+  {
+    "revision": "43f74a7a2225f6c9c856a4a920b866c5",
+    "url": "/sannilares.github.io/static/media/sanni.43f74a7a.jpg"
+  },
+  {
+    "revision": "8e186bf4f22e801be17d2fed1dd0de8f",
+    "url": "/sannilares.github.io/static/media/uncover.8e186bf4.webm"
+  },
+  {
+    "revision": "0ce8c722f7c124f71bd82836c5b63017",
+    "url": "/sannilares.github.io/static/media/videoImg.0ce8c722.png"
+  },
+  {
+    "revision": "54549db4b61a12ca27b46f175419504a",
+    "url": "/sannilares.github.io/static/media/videoImg2.54549db4.png"
+  }
+]);
